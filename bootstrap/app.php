94ab96e49ce2c0782842293d100e4520ef1a6442
Setup <setup@skillcheck.local>
2026-09-25 22:29:52 +0700

    feat: configure CORS for Vercel frontend origin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Fruitcake\Cors\HandleCors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
